342462b41cb7640518354c44c73cccc104a1a510
vladislav <vlad@itpw.ru>
2022-04-13 17:59:05 +0300

    init commit

--- src/views/OrderItem/OrderItem.jsx
+++ src/views/OrderItem/OrderItem.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Card} from "react-bootstrap";
+import {useParams} from "react-router-dom";
+import api from "../../plugins/axios/api";
 
 const OrderItem = () => {
+    const [orderInfo,setOrderInfo] = useState()
+    const urlParams = useParams()
+    useEffect(()=>{
+        api(`marketplace/order/${urlParams.id}/`)
+            .then((response)=>{
+                setOrderInfo(response.data)
+            })
+    },[])
     return (
-        <div>
-            123
-        </div>
+        <Card>
+            <Card.Header>
+                <Card.Title>Заказ #{urlParams.id}</Card.Title>
+            </Card.Header>
+            <Card.Body>
+                <div>
+                    <p>Статус: {urlParams.status}</p>
+                </div>
+            </Card.Body>
+        </Card>
     );
 };
 
