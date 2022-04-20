6b9b9170f6a054c8bdd956a529e4e8b5943fa0e1
Vladislav <32987720+xxlifestyle@users.noreply.github.com>
2022-04-20 10:52:23 +0300

    Отображение адреса заказа на карте (OrderItem)

--- src/components/OrderMap/OrderMap.jsx
+++ src/components/OrderMap/OrderMap.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import styles from './OrderMap.module.css'
 import Map, {Marker, Popup} from "react-map-gl";
 import {Button, Modal} from "react-bootstrap";
+import styles from './OrderMap.module.css'
 
 const OrderMap = (props) => {
     return (
         <div>
             <Modal show={true}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title>Адрес заказа</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     {props.orderInfo &&
                         <Map
                             initialViewState={{
-                                longitude: 30.3158,
-                                latitude: 59.93901,
+                                longitude: props.orderInfo.longitude,
+                                latitude: props.orderInfo.latitude,
                                 zoom: 14
                             }}
                             style={{width: '100%', height: 400}}
                             mapStyle="mapbox://styles/mapbox/streets-v9"
                         >
+                            <div className={styles.controlPanel}>
+
+                                <span><b>Адрес:</b> {props.orderInfo.address}</span>
+                                {/*<span>{selectedShop.description}</span>*/}
+                            </div>
                             {/*{showPopup && selectedCoord !== null && (*/}
                             {/*    <Popup longitude={selectedCoord.longitude} latitude={selectedCoord.latitude}*/}
                             {/*           anchor="bottom"*/}
