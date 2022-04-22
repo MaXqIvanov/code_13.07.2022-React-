71d5cca7274f17550f674355af0fb6e3dfd3cd4d
Vladislav <32987720+xxlifestyle@users.noreply.github.com>
2022-04-22 13:13:49 +0300

    Исключение обработки Babel'ем mapbox-gl зависимости

--- src/components/DeliveryMap/DeliveryMap.jsx
+++ src/components/DeliveryMap/DeliveryMap.jsx
@@ -1,7 +1,8 @@
 import React, {useEffect, useRef, useState} from 'react';
 import marker from '../../assets/map-marker.png'
 import styles from './DeliveryMap.module.css'
-import mapboxgl from 'mapbox-gl';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import mapboxgl from '!mapbox-gl';
 import Map, {Marker, Popup,ControlPanel} from "react-map-gl";
 import axios from "axios"; // eslint-disable-line import/no-webpack-loader-syntax
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGllbGl0IiwiYSI6ImNreGJ3b3RlOTByOHQycHE5bWwzaXlxZ2cifQ.1QkwgR8DegdGymUZL3iTjg';
@@ -40,6 +41,7 @@ const DeliveryMap = (props) => {
                 zoom: 14
             }}
             onClick={(e)=>{selectCoord(e)}}
+            mapboxAccessToken={token}
             style={{width: '100%', height: 400}}
             mapStyle="mapbox://styles/mapbox/streets-v9"
         >
