7bcb47c80f167a9a73a340d1afb529bb5c18feb7
“MaXqIvanov” <maksivanov35@ya.ru>
2022-07-13 11:46:11 +0300

    update

--- src/components/OrdersPage/ModalAddressAdded.tsx
+++ src/components/OrdersPage/ModalAddressAdded.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from '../../scss/OrdersPage.module.scss';
 
-export const ModalAddressAdded = () => {
+export const ModalAddressAdded = ({setVisibleAddress}:any) => {
   return (
     <div className={styles.address_added}>
         <div className={styles.address_added_wrapper}>
             добавление адресса
+            <i onClick={()=>setVisibleAddress(false)} className={`bi bi-x ${styles.close_btn}`}></i>
         </div>
     </div>
   )
